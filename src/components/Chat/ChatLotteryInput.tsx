9750a02c54f725a11884bb97fe282658e31aca59
Andrii Rastorhuiev <andrey.r@itsua.org>
2023-02-21 12:14:49 +0200

    fix issues, add outlined variant

--- src/components/Chat/ChatLotteryInput.tsx
+++ src/components/Chat/ChatLotteryInput.tsx
@@ -1,18 +1,40 @@
+import { ChangeEvent, FormEvent, useState } from 'react'
+
+import { InputWithLabel } from '../base/InputWithLabel'
 import { Button } from '../common/Button/Button'
 import { DiamondIcon } from '../DiamondIcon/DiamondIcon'
 
 export const ChatLotteryInput = () => {
+  const [tipCoins, setTipCoins] = useState('')
+
+  const handleTipRain = (event: ChangeEvent<HTMLInputElement>) => {
+    setTipCoins(event.target.value)
+  }
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log('rain from coins')
+    setTipCoins('')
+  }
+
   return (
-    <div className='relative z-10 rounded border bg-green-primary/15 border-green-primary border-dashed flex items-center justify-between p-1.5'>
+    <form
+      onSubmit={handleSubmit}
+      className='relative z-10 rounded border bg-green-primary/15 border-green-primary border-dashed flex items-center justify-between p-1.5'
+    >
       <span className='w-5 h-5 text-center leading-6 shrink-0 bg-green-primary/20 rounded relative mr-2 text-green-primary'>
         <DiamondIcon className='-inset-full absolute m-auto' />
       </span>
-      <input
+      <InputWithLabel
         type='text'
-        className='group grow w-0 mr-2 bg-transparent bg-none border-none outline-none shadow-none appearance-none m-0'
         placeholder='...'
+        variant='OUTLINED'
+        value={tipCoins}
+        onChange={handleTipRain}
       />
-      <Button variant='GRADIENT'>Tip Rain</Button>
-    </div>
+      <Button variant='GRADIENT' size='SMALL' type='submit'>
+        Tip Rain
+      </Button>
+    </form>
   )
 }
--- src/components/base/InputWithLabel.tsx
+++ src/components/base/InputWithLabel.tsx
@@ -3,7 +3,8 @@ import clsx from 'clsx'
 
 enum InputVariantEnum {
   BASE = 'BASE',
-  CHAT = 'CHAT'
+  CHAT = 'CHAT',
+  OUTLINED = 'OUTLINED'
 }
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -22,8 +23,12 @@ export const InputWithLabel: FC<InputProps> = ({
     'mb-8': variant === InputVariantEnum.BASE
   })
   const inputClasses = clsx('relative z-10', {
-    'grow w-0 mr-2 bg-transparent bg-none border-none outline-none shadow-none': variant === InputVariantEnum.BASE,
-    'rounded p-3 pl-4 pr-14 bg-blue-secondary text-gray_96 w-full text-13 focus:outline focus:outline-pink-primary': variant === InputVariantEnum.CHAT
+    'grow w-0 mr-2 bg-transparent bg-none border-none outline-none shadow-none':
+      variant === InputVariantEnum.BASE,
+    'rounded p-3 pl-4 pr-14 bg-blue-secondary text-gray_96 w-full text-13 focus:outline focus:outline-pink-primary':
+      variant === InputVariantEnum.CHAT,
+    'group grow w-0 mr-2 bg-transparent bg-none border-none outline-none shadow-none appearance-none m-0 w-full':
+      variant === InputVariantEnum.OUTLINED
   })
 
   return (
@@ -33,8 +38,18 @@ export const InputWithLabel: FC<InputProps> = ({
           {label}
         </div>
       )}
-      <div className={variant === InputVariantEnum.BASE ? 'bg-dark/25 rounded-xl rounded-tl-none overflow-hidden' : ''}>
-        <div className={variant === InputVariantEnum.BASE ? 'gradient-blue-secondary flex py-2.5 pl-4 pr-3' : ''}>
+      <div
+        className={
+          variant === InputVariantEnum.BASE
+            ? 'bg-dark/25 rounded-xl rounded-tl-none overflow-hidden'
+            : ''
+        }
+      >
+        <div
+          className={
+            variant === InputVariantEnum.BASE ? 'gradient-blue-secondary flex py-2.5 pl-4 pr-3' : ''
+          }
+        >
           <input
             type={inputProps.type}
             name={inputProps.name}
