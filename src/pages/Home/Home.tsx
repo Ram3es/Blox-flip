2ad321f6c8f2960b868cb930869702b4c8b78a7a
Andrii Rastorhuiev <andrey.r@itsua.org>
2023-02-27 11:24:42 +0200

    fix homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,12 @@
 import GamesSection from '../../components/home-page/GamesSection'
 import SignInModal from '../../components/modal/SignInModal.'
 import { LiveFeed } from '../../components/LiveFeed/LiveFeed'
-import RobuxModal from '../../components/modal/RobuxModal'
 
 export const Home = () => {
   return (
     <div>
-      {/* <GamesSection />
-      <SignInModal /> */}
+      <GamesSection />
+      <SignInModal />
       <LiveFeed />
     </div>
   )
