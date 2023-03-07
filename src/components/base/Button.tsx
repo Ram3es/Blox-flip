ae6f3de3713db99df0d783ac307389b0d55066ea
Andrii Rastorhuiev <andrey.r@itsua.org>
2023-03-07 11:39:04 +0200

    add one more button variant

--- src/components/base/Button.tsx
+++ src/components/base/Button.tsx
@@ -4,13 +4,15 @@ import clsx from 'clsx'
 enum VariantEnum {
   Standard = 'Standard',
   Gradient = 'Gradient',
-  Outlined = 'Outlined'
+  Outlined = 'Outlined',
+  Highlight = 'Highlight'
 }
 
 enum ColorEnum {
   GreenPrimary = 'GreenPrimary',
   BlueSecondary = 'BlueSecondary',
-  BlueAccent = 'BlueAccent'
+  BlueAccent = 'BlueAccent',
+  BlueHighlight = 'BlueHighlight'
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -24,12 +26,14 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
     const colorClasses = clsx('', {
       'bg-green-primary': color === ColorEnum.GreenPrimary,
       'bg-blue-secondary': color === ColorEnum.BlueSecondary,
-      'bg-blue-accent': color === ColorEnum.BlueAccent
+      'bg-blue-accent': color === ColorEnum.BlueAccent,
+      'bg-blue-highlight': color === ColorEnum.BlueHighlight
     })
 
     const variantClasses = clsx('font-bold flex items-center rounded', {
       'gradient-green hover:bg-gradient-to-r hover:to-green-500 hover:from-green-500 shadow-green-20': variant === VariantEnum.Gradient,
-      'text-green-primary border bg-green-primary/15 hover:bg-green-primary/30 border-green-primary  ': variant === VariantEnum.Outlined
+      'text-green-primary border bg-green-primary/15 hover:bg-green-primary/30 border-green-primary ': variant === VariantEnum.Outlined,
+      'text-gray-primary font-semibold rounded bg-blue-highlight border border-blue-highlight hover:text-white': variant === VariantEnum.Highlight
     })
 
     return (
