ec80ffd07528fb26800bdb47f97c761dc0a7f4a0
Andrii Rastorhuiev <andrey.r@itsua.org>
2023-03-07 22:53:59 +0200

    fix functions position

--- src/helpers/leaderboardHelpers.ts
+++ src/helpers/leaderboardHelpers.ts
@@ -1,9 +1,9 @@
-export const getTopThreeUsers = <T, K extends keyof T>(users: T[], field: K): T[] => {
-  return getSortedUsersByField(users, field).slice(0, 3)
-}
-
 export const getSortedUsersByField = <T, K extends keyof T>(users: T[], field: K): T[] => {
   return users.sort((a, b) => Number(b[field]) - Number(a[field]))
+}
+
+export const getTopThreeUsers = <T, K extends keyof T>(users: T[], field: K): T[] => {
+  return getSortedUsersByField(users, field).slice(0, 3)
 }
 
 export const getPlaceByIndex = (index: number): 1 | 2 | 3 => {
