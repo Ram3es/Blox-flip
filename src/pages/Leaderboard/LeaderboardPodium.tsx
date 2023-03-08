9b68422c486e5f19084e6ae035a302df200759fd
Andrii Rastorhuiev <andrey.r@itsua.org>
2023-03-08 10:53:54 +0200

    add placeholder if no data

--- src/pages/Leaderboard/LeaderboardPodium.tsx
+++ src/pages/Leaderboard/LeaderboardPodium.tsx
@@ -7,9 +7,23 @@ import { PodiumItem } from './PodiumItem'
 export const LeaderboardPodium = () => {
   const [topThreeUsers] = useState<ISecondUser[]>([...getTopThreeUsers(users, 'level')])
 
+  const fakeUsers: ISecondUser[] = new Array(3 - topThreeUsers.length).fill({
+    isWinner: true,
+    game: 'game 64',
+    date: '2047-06-14T19:24:08.234Z',
+    bet: 0,
+    rate: 0,
+    profit: 0,
+    username: 'No data',
+    avatar: '',
+    level: 0,
+    id: '64'
+  })
+  const usersToRender = [...topThreeUsers, ...fakeUsers]
+
   return (
     <div className='flex flex-wrap items-end'>
-      {topThreeUsers.map((user, index) => (
+      {usersToRender.map((user, index) => (
         <PodiumItem key={user.id} user={user} place={getPlaceByIndex(index)} />
       ))}
     </div>
