78d356534ddbc169d36eeb1f16f93e1ac13bf9e3
Andrii Rastorhuiev <andrey.r@itsua.org>
2023-03-15 17:07:01 +0200

    add deposit crypto

---
+++ src/components/common/CryptoCalculator.tsx
@@ -0,0 +1,74 @@
+import { FC } from 'react'
+import { Input } from './Input/Input'
+
+import BitcoinIconSmall from '../../assets/img/deposit_bitcoin_small.png'
+import InputWithLabel from '../base/InputWithLabel'
+import { DiamondIcon } from '../DiamondIcon/DiamondIcon'
+
+const BridgeComponent = () => {
+  return (
+    <div className='hidden xl:block space-y-[2px]'>
+      <div className='box-border h-[0.1875rem] w-2.5 bg-dark/25'>
+        <div className='h-[0.1875rem] gradient-blue-secondary'></div>
+      </div>
+      <div className='box-border h-[0.1875rem] w-2.5 bg-dark/25'>
+        <div className='h-[0.1875rem] gradient-blue-secondary'></div>
+      </div>
+    </div>
+  )
+}
+
+interface CryptoCalculatorProps {
+  any?: any
+}
+
+export const CryptoCalculator: FC<CryptoCalculatorProps> = () => {
+  return (
+    <div className='flex flex-wrap mb-5 z-20'>
+      <div className='w-full xs:w-1/3 grow mb-4 relative'>
+        <div className='bg-dark/25 rounded-xl mt-10'>
+          <Input type='text' variant='FORM' value='1,500' placeholder='...' />
+        </div>
+        <div className='w-5 h-5 text-center leading-6 shrink-0 bg-green-primary/20 rounded mr-2 text-green-primary absolute top-[58px] left-4'>
+          <DiamondIcon className='-inset-full absolute m-auto' />
+        </div>
+      </div>
+      <div className='px-2.5 w-full xs:w-1/3 md:w-auto grow shrink-0 mb-4 relative '>
+        <InputWithLabel
+          type='text'
+          label='Calculator'
+          labelClasses='flex flex-col w-full mb-4 items-center'
+          titleClasses='gradient-blue-secondary text-gray-primary rounded-t-xl py-2 text-center w-52 inline-block'
+          inputWrapperClasses='bg-dark/25 rounded-xl overflow-hidden w-full'
+          inputClasses='overflow-ellipsis grow w-0 mr-2 bg-transparent bg-none border-none outline-none w-full shadow-none leading-5 ml-5 mr-28 truncate'
+          placeholder='...'
+          value='500.29'
+        />
+        <span className='min-w-fit shrink-0 absolute top-[44px] left-6 bg-none border-none outline-none shadow-none py-3 text-green-secondary font-extrabold mr-1.5'>
+          $
+        </span>
+        <div className='absolute right-0 top-16'>
+          <BridgeComponent />
+        </div>
+        <div className='absolute left-0 top-16'>
+          <BridgeComponent />
+        </div>
+      </div>
+      <div className='w-full xs:w-1/3 grow mb-4 relative'>
+        <div className='bg-dark/25 rounded-xl mt-10'>
+          <Input type='text' variant='FORM' value='0.1398582' placeholder='...' />
+        </div>
+        <span className='min-w-fit shrink-0 absolute left-4 top-[59px]'>
+          <img
+            src={BitcoinIconSmall}
+            alt=''
+            width='18'
+            height='18'
+            loading='lazy'
+            decoding='async'
+          />
+        </span>
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/Deposit/DepositCrypto.tsx
@@ -0,0 +1,85 @@
+import { FC } from 'react'
+import { Button } from '../../components/base/Button'
+import { CopyIconSecond } from '../../components/icons/CopyIconSecond'
+
+import InputWithLabel from '../../components/base/InputWithLabel'
+import BitcoinIconSmall from '../../assets/img/deposit_bitcoin_small.png'
+import QRCodePlaceHolder from '../../assets/img/qr-kod.png'
+import { CryptoCalculator } from '../../components/common/CryptoCalculator'
+
+interface DepositCryptoProps {
+  any?: any
+}
+
+export const DepositCrypto: FC<DepositCryptoProps> = () => {
+  return (
+    <div className='border-t border-b border-t-sky-primary/40 border-b-sky-primary/40 rounded mb-9'>
+      <div className='border--mask border--radial-blue  bg-gradient-radial from-blue-light-secondary/20 to-blue-accent-secondary/0 rounded text-sm px-3 xxs:px-6 py-9 overflow-hidden relative'>
+        <div className='relative z-20 text-base'>
+          <div className='flex flex-wrap -mx-2 mb-5'>
+            <div className='relative px-2 w-full xs:w-1/2 md:w-auto grow shrink-0 mb-4'>
+              <InputWithLabel
+                type='text'
+                name='affiliate'
+                label='Bitcoin Address'
+                labelClasses='flex flex-col w-full mb-4 items-center'
+                titleClasses='gradient-blue-secondary text-gray-primary rounded-t-xl py-2 px-5 w-52 text-center inline-block'
+                inputWrapperClasses='bg-dark/25 rounded-xl overflow-hidden w-full'
+                inputClasses='overflow-ellipsis grow w-0 mr-2 bg-transparent bg-none border-none outline-none shadow-none leading-5 py-2 mr-12 truncate'
+                placeholder='...'
+                readOnly
+                value='0x351af6d2387a0b2cf9af41sSDFw43585d2ce2a25'
+              />
+              <div className='absolute z-20 top-[60px] right-4'>
+                <Button className='w-7 shrink-0' type='button'>
+                  <CopyIconSecond />
+                </Button>
+              </div>
+            </div>
+            <div className='relative px-2 w-full xs:w-1/2 md:w-auto grow shrink-0 mb-4'>
+              <InputWithLabel
+                type='text'
+                autoComplete='off'
+                label='Send Amount'
+                labelClasses='flex flex-col w-full mb-4 items-center'
+                titleClasses='gradient-blue-secondary text-gray-primary rounded-t-xl py-2 text-center w-52 inline-block'
+                inputWrapperClasses='bg-dark/25 rounded-xl overflow-hidden w-full'
+                inputClasses='overflow-ellipsis grow w-0 mr-2 bg-transparent bg-none border-none outline-none shadow-none leading-5 py-2 ml-7 mr-28 truncate'
+                placeholder='...'
+                value='0.1398582'
+              />
+              <span className='min-w-fit shrink-0 absolute top-[59px] left-6'>
+                <img
+                  src={BitcoinIconSmall}
+                  alt=''
+                  width='18'
+                  height='18'
+                  loading='lazy'
+                  decoding='async'
+                />
+              </span>
+            </div>
+          </div>
+          <CryptoCalculator />
+          <div className='flex flex-wrap xs:flex-nowrap justify-center xs:justify-start items-center'>
+            <div className='min-w-fit shrink-0 w-28 mx-3 mb-6 xs:mb-0'>
+              <img
+                src={QRCodePlaceHolder}
+                alt=''
+                width='115'
+                height='115'
+                loading='lazy'
+                decoding='async'
+              />
+            </div>
+            <span className='text-gray-primary text-center grow lg:pr-36'>
+              Send only Bitcoin to the address above, or QR code to the left. Do not send any other
+              crypto currency. <br />6 confirmations are required before you are credited to your
+              balance.
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
