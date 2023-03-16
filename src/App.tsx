d4e3d83f28c7ffcbea0b47b1ae0e62c9b2833bed
Andrii Rastorhuiev <andrey.r@itsua.org>
2023-03-16 14:55:00 +0200

    resolve conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,8 @@ import { Withdraw } from './pages/Withdraw/Withdraw'
 import { Leaderboard } from './pages/Leaderboard/Leaderboard'
 import BattleLobby from './pages/Battle/BattleLobby'
 import { DepositRobux } from './pages/Deposit/DepositRobux'
+import { CaseOpening } from './pages/Cases/CaseOpening/CaseOpening'
+import { DepositCrypto } from './pages/Deposit/DepositCrypto'
 
 export const App = () => {
   return (
@@ -39,7 +41,11 @@ export const App = () => {
           <Route path='/crash' element={<Wheel />} />
           <Route path='/king' element={<King />} />
           <Route path='/plinko' element={<Plinko />} />
-          <Route path='/deposit' element={<Deposit />} />
+          <Route path='/deposit' element={<Deposit />}>
+            <Route path='robux' element={<DepositRobux />} />
+
+            <Route path='crypto' element={<DepositCrypto />} />
+          </Route>
           <Route path='/withdraw' element={<Withdraw />}>
             <Route path='robux' element={<Robux />} />
             <Route path='roblox-limiteds' element={<RobloxLimiteds />} />
