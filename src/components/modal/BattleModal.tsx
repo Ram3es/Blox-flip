f3c585448eb140c2363df843d6aa87d14db8fbb7
Roman Luzan <romaluzan1@gmail.com>
2023-03-17 13:23:09 +0200

    uncomit

--- src/components/modal/BattleModal.tsx
+++ src/components/modal/BattleModal.tsx
@@ -1,23 +1,31 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { unboxCard } from '../../mocks/cards'
 import { IItemCard } from '../../types/itemCard'
-import { Button } from '../base/Button'
-// import { searchData } from '../../helpers/searchData'
-import ModalWrapper from '../base/ModalWrapper'
+import { Button } from '../Base/Button'
+import ModalWrapper from '../Base/ModalWrapper'
+import { searchData } from '../../helpers/searchData'
 import UnboxingCard from '../base/UnboxingCard'
-import GreenTipSelect from '../common/GreenTipSelect'
-import { QuantityCoinsWithChildren } from '../common/QuantityCoins/QuantityWithChildren'
-import SearchInput from '../common/SearchInput'
-import DaggersGreenGradient from '../icons/DaggersGreenGradient'
+import GreenTipSelect from '../Common/GreenTipSelect'
+import { QuantityCoinsWithChildren } from '../Common/QuantityCoins/QuantityWithChildren'
+import SearchInput from '../Common/SearchInput'
+import DaggersGreenGradient from '../Icons/DaggersGreenGradient'
+import { useToolbarState } from '../../helpers/hooks/useTollbarState'
 
 const BattleModal = ({ isOpen, onClose }: { isOpen: boolean, onClose: Function }) => {
   const [unboxCards, setAllCards] = useState<IItemCard[]>(unboxCard)
   const [selectedCards, setSelected] = useState<IItemCard[]>([])
-  // const { priceRange, searchBy } = useToolbarState()
+  const {
+    value,
+    searchBy,
+    priceRange,
+    onChange,
+    setPriceRange
+  } = useToolbarState()
+  console.log(priceRange, priceRange)
 
   const totalPriceSelected = selectedCards.reduce((acc, item) => acc + item.price, 0)
-  // const ranged = useMemo(() => unboxCards.filter((card) => card.price >= priceRange.from && card.price <= priceRange.to), [priceRange, unboxCards])
-  // const filtered = useMemo(() => searchData(ranged, 'name', searchBy), [searchBy, unboxCards, ranged])
+  const ranged = useMemo(() => unboxCards.filter((card) => card.price >= priceRange.from && card.price <= priceRange.to), [priceRange, unboxCards])
+  const filtered = useMemo(() => searchData(ranged, 'name', searchBy), [searchBy, unboxCards, ranged])
 
   const onSelect = (card: IItemCard) => {
     setSelected(state => ([...state, card]))
@@ -35,13 +43,13 @@ const BattleModal = ({ isOpen, onClose }: { isOpen: boolean, onClose: Function }
             <h3 className='text-2xl font-bold ml-3 '>Battle Creation</h3>
           </div>
           <div className=' w-full flex justify-center xs:justify-end gap-5 mt-5 xs:mt-0 xs:gap-2'>
-            <GreenTipSelect onSelect={() => {}} />
-            <SearchInput placeholder='Search cases' value={''} onChange={() => {}} />
+            <GreenTipSelect onSelect={setPriceRange} />
+            <SearchInput placeholder='Search cases' value={value} onChange={onChange} />
           </div>
         </div>
         <div className=" border-b border-blue-accent-secondary" />
-        <div className='w-full max-h-[calc(100vh_-_210px)]  mt-5 mb-[34px] flex flex-wrap  overflow-auto scrollbar-thumb-blue-secondary scrollbar-track-blue-darken/40 scrollbar-thin scrollbar-track-rounded-full scrollbar-thumb-rounded-full  pr-3 -mr-2 '>
-            {unboxCards.map((card) => (
+        <div className='w-full min-h-[232px] max-h-[calc(100vh_-_210px)]  mt-5 mb-[34px] flex flex-wrap  overflow-auto scrollbar-thumb-blue-secondary scrollbar-track-blue-darken/40 scrollbar-thin scrollbar-track-rounded-full scrollbar-thumb-rounded-full  pr-3 -mr-2 '>
+            {filtered.map((card) => (
                 <UnboxingCard
                    key={card.id}
                    id={card.id}
@@ -58,7 +66,7 @@ const BattleModal = ({ isOpen, onClose }: { isOpen: boolean, onClose: Function }
               </div>
             </div>
             <Button
-              onClick={() => {} }
+              onClick={() => onClose() }
               className='bg-green-primary hover:bg-green-500  border border-green-primary py-2 px-4 leading-4 rounded '
             >
                 Complete
