5d57ae64d0fb11eb148446ff51da3f413ac578bf
Andrii Rastorhuiev <andrey.r@itsua.org>
2023-03-21 15:26:16 +0200

    add bad responsive

--- src/pages/Plinko/Plinko.tsx
+++ src/pages/Plinko/Plinko.tsx
@@ -1,13 +1,13 @@
 import BetActions from './BetActions/BetActions'
 import GamePlinko from './Game/GamePlinko'
 
-export default function Plinko () {
+export default function Plinko() {
   return (
-    <div className='flex justify-center'>
-      <div className='bg-blue-accent rounded-lg w-1/4 py-6 mr-4'>
+    <div className='flex flex-col md:flex-row justify-center'>
+      <div className='bg-blue-accent rounded-lg md:w-1/4 w-full py-6 mr-4'>
         <BetActions />
       </div>
-      <div className='bg-blue-primary rounded-lg w-3/4 flex items-start justify-center'>
+      <div className='bg-blue-primary rounded-lg md:w-3/4 w-full flex items-start justify-center'>
         <GamePlinko />
       </div>
     </div>
