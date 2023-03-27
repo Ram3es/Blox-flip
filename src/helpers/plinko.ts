02a25bd4b9d71b9d7a43ff8f2846a22d36726905
Andrii Rastorhuiev <andrey.r@itsua.org>
2023-03-27 11:52:01 +0300

    worked but not good margin padding column size, and transition

--- src/helpers/plinko.ts
+++ src/helpers/plinko.ts
@@ -1,3 +1,6 @@
+import { RiskVariant } from '../types/enums'
+import { RowVariant } from '../types/Plinko'
+
 export const getRowSettingsByRows = (rows: number) => {
   const PEG_SIZE = 5
   const PLINKO_SIZE = 9
@@ -9,45 +12,79 @@ export const getRowSettingsByRows = (rows: number) => {
       return {
         pegSize: PEG_SIZE,
         plinkoSize: PLINKO_SIZE,
-        yForce: Y_FORCE_BASE * 1.2 * -1,
-        xForce: X_FORCE_BASE * 1.02
+        yForce: Y_FORCE_BASE * 1.3,
+        xForce: X_FORCE_BASE * 1
       }
     case 10:
       return {
         pegSize: PEG_SIZE,
         plinkoSize: PLINKO_SIZE,
-        yForce: Y_FORCE_BASE * 1.05 * -1,
-        xForce: X_FORCE_BASE * 0.95
+        yForce: Y_FORCE_BASE * 1,
+        xForce: X_FORCE_BASE * 1
       }
     case 12:
       return {
         pegSize: 4,
         plinkoSize: 8,
-        yForce: Y_FORCE_BASE * 1.05 * -1,
+        yForce: Y_FORCE_BASE * 1.05,
         xForce: X_FORCE_BASE * 0.84
       }
     case 14:
       return {
         pegSize: 3.5,
         plinkoSize: 5,
-        yForce: Y_FORCE_BASE * 0.967 * -1,
+        yForce: Y_FORCE_BASE * 0.967,
         xForce: X_FORCE_BASE * 0.7757
       }
     case 16:
       return {
         pegSize: 2,
         plinkoSize: 4.5,
-        yForce: Y_FORCE_BASE * 0.95 * -1,
+        yForce: Y_FORCE_BASE * 0.95,
         xForce: X_FORCE_BASE * 0.7465
       }
     default:
       return {
         pegSize: PEG_SIZE,
         plinkoSize: PLINKO_SIZE,
-        yForce: Y_FORCE_BASE * -1,
+        yForce: Y_FORCE_BASE,
         xForce: X_FORCE_BASE
       }
   }
+}
+
+export const getMultipliersByProps = (
+  risk: keyof typeof RiskVariant,
+  row: RowVariant
+): [] | number[] => {
+  const multipliers = {
+    Low: {
+      8: [0.48, 0.96, 1.06, 2.01, 5.37],
+      10: [0.48, 0.96, 1.06, 1.34, 2.88, 8.54],
+      12: [0.48, 0.96, 1.06, 1.34, 1.54, 2.88, 9.6],
+      14: [0.48, 0.96, 1.06, 1.25, 1.34, 1.82, 3.84, 6.81],
+      16: [0.48, 0.96, 1.06, 1.15, 1.34, 1.34, 1.92, 8.64, 15.3]
+    },
+    Medium: {
+      8: [0.38, 0.67, 1.25, 2.88, 12.5],
+      10: [0.38, 0.58, 1.34, 1.92, 4.8, 21.1],
+      12: [0.29, 0.58, 1.06, 1.92, 3.84, 10.6, 31.7],
+      14: [0.19, 0.48, 0.96, 1.82, 3.84, 6.72, 14.4, 55.6],
+      16: [0.29, 0.48, 0.96, 1.44, 2.88, 4.8, 9.6, 39.3, 105.5]
+    },
+    High: {
+      8: [0.19, 0.29, 1.44, 3.84, 27.8],
+      10: [0.19, 0.29, 0.86, 2.88, 9.6, 72.9],
+      12: [0.19, 0.19, 0.67, 1.92, 7.77, 23.0, 163.1],
+      14: [0.19, 0.19, 0.29, 1.82, 4.8, 17.3, 53.7, 403.0],
+      16: [0.19, 0.19, 0.19, 1.92, 3.84, 8.64, 24.9, 124.7, 959.5]
+    }
+  }
+
+  if (multipliers?.[risk] && multipliers?.[risk]?.[row]) {
+    return multipliers[risk][row]
+  }
+  return []
 }
 
 export const getRandomPathByRows = (rows: number): number[] => {
--- src/pages/Plinko/Plinko2/PlinkoGame2.tsx
+++ src/pages/Plinko/Plinko2/PlinkoGame2.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import { Bodies, Body, Engine, Events, IEventCollision, Render, Runner, World } from 'matter-js'
 import clsx from 'clsx'
@@ -9,6 +9,7 @@ import { RowVariant } from '../../../types/Plinko'
 import { RiskVariant } from '../../../types/enums'
 import {
   getColorByMultiplier,
+  getMultipliersByProps,
   getRandomPathByRows,
   getRowSettingsByRows
 } from '../../../helpers/plinko'
@@ -22,7 +23,7 @@ const mainConfig = {
   contour: 50,
   startPins: 3
 }
-type PathMap = Record<number, unknown>
+type PathMap = Record<number, number[] | undefined>
 
 interface PlinkoGame2Props {
   rows: RowVariant
@@ -31,14 +32,16 @@ interface PlinkoGame2Props {
 }
 
 const PlinkoGame2 = ({ rows, risk, numberOfBets }: PlinkoGame2Props) => {
+  // const [ballCache, setBallCache] = useState<PathMap>({})
+
   const plinkoGameRef = useRef<HTMLDivElement | null>(null)
   const multiplierRefs = useRef<Array<HTMLDivElement | null>>([])
   const engine = Engine.create()
-  console.log(multiplierRefs)
   let columnSize = Math.round(mainConfig.width / (rows + 2))
   let rowSize = mainConfig.height / rows
 
   const paths: PathMap = {}
+  // const [paths, setPaths] = useState<PathMap>({})
 
   let forceCache: any = []
   let ballCache: PathMap = {}
@@ -52,6 +55,71 @@ const PlinkoGame2 = ({ rows, risk, numberOfBets }: PlinkoGame2Props) => {
       Body.setStatic(ball.body, false)
     }
     forceCache = []
+  }
+
+  const handleCollision = (event: IEventCollision<Engine>) => {
+    const { pairs } = event
+
+    pairs.forEach((pair, i) => {
+      const { bodyA, bodyB } = pair
+      const { label: labelA } = bodyA
+      const { label: labelB } = bodyB
+
+      if (labelA !== labelB) {
+        if (labelB === 'plinko') {
+          if (!ballCache[bodyB.id]) {
+            ballCache[bodyB.id] = 0
+          }
+          ballCache[bodyB.id]++
+
+          const shiftedX = (mainConfig.width / 2 - bodyB.position.x) % (columnSize / 2)
+          const shiftedY =
+            (-bodyB.position.y + (16 - (rowSettings.pegSize + rowSettings.plinkoSize))) % rowSize
+          const newX =
+            Math.abs(shiftedX) < columnSize / 4
+              ? shiftedX
+              : columnSize / 2 + shiftedX * (shiftedX < 0 ? 1 : -1)
+
+          const newY = shiftedY
+          Body.setPosition(bodyB, {
+            x: bodyB.position.x + newX,
+            y: bodyB.position.y + newY
+          })
+
+          forceCache.push({
+            body: bodyB,
+            force: {
+              x: rowSettings.xForce * (paths[bodyB.id][ballCache[bodyB.id] - 1] === 1 ? 1 : -1),
+              y: rowSettings.yForce
+            }
+          })
+
+          if (
+            labelA === 'BottomWall' ||
+            labelA === 'Rectangle Body' ||
+            labelA === 'LeftWall' ||
+            labelA === 'RightWall'
+          ) {
+            const rights = paths[bodyB.id].filter((val: number) => val === 1).length
+            const i = (rights - (paths[bodyB.id].length - rights)) / 2 + paths[bodyB.id].length / 2
+            const multiplierBox2 = multiplierRefs.current[i]
+
+            if (multiplierBox2?.style) {
+              multiplierBox2.style.transform = 'translateY(10px)'
+
+              setTimeout(() => {
+                multiplierBox2.style.transform = 'translateY(0px)'
+              }, 1000)
+            }
+
+            World.remove(engine.world, bodyB)
+            delete paths[bodyB.id]
+
+            return
+          }
+        }
+      }
+    })
   }
 
   useEffect(() => {
@@ -113,7 +181,7 @@ const PlinkoGame2 = ({ rows, risk, numberOfBets }: PlinkoGame2Props) => {
       render.canvas.remove()
       render.textures = {}
     }
-  }, [rows, risk])
+  }, [risk, rows])
 
   const leftWall = Bodies.rectangle(0, 0, 133.33333333333334 / 2, mainConfig.width * 2, {
     isStatic: true,
@@ -160,6 +228,7 @@ const PlinkoGame2 = ({ rows, risk, numberOfBets }: PlinkoGame2Props) => {
       restitution: 0,
       friction: 1,
       mass: 0.23805846,
+      inverseMass: 1 / 0.23805846,
       collisionFilter: {
         group: -1
       },
@@ -178,9 +247,6 @@ const PlinkoGame2 = ({ rows, risk, numberOfBets }: PlinkoGame2Props) => {
     const path = getRandomPathByRows(rows)
     console.log(path)
     paths[plinko.id] = path
-    // paths[plinko.id] = [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0] // 12
-    // paths[plinko.id] = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] // 16
-
     World.add(engine.world, plinko)
   }
   const makePeg = (x: number, y: number) => {
@@ -191,104 +257,6 @@ const PlinkoGame2 = ({ rows, risk, numberOfBets }: PlinkoGame2Props) => {
       friction: 1,
       render: { fillStyle: '#4F5677' },
       label: 'peg'
-    })
-  }
-  const getMultipliersByProps = (
-    risk: keyof typeof RiskVariant,
-    row: RowVariant
-  ): [] | number[] => {
-    const multipliers = {
-      Low: {
-        8: [0.48, 0.96, 1.06, 2.01, 5.37],
-        10: [0.48, 0.96, 1.06, 1.34, 2.88, 8.54],
-        12: [0.48, 0.96, 1.06, 1.34, 1.54, 2.88, 9.6],
-        14: [0.48, 0.96, 1.06, 1.25, 1.34, 1.82, 3.84, 6.81],
-        16: [0.48, 0.96, 1.06, 1.15, 1.34, 1.34, 1.92, 8.64, 15.3]
-      },
-      Medium: {
-        8: [0.38, 0.67, 1.25, 2.88, 12.5],
-        10: [0.38, 0.58, 1.34, 1.92, 4.8, 21.1],
-        12: [0.29, 0.58, 1.06, 1.92, 3.84, 10.6, 31.7],
-        14: [0.19, 0.48, 0.96, 1.82, 3.84, 6.72, 14.4, 55.6],
-        16: [0.29, 0.48, 0.96, 1.44, 2.88, 4.8, 9.6, 39.3, 105.5]
-      },
-      High: {
-        8: [0.19, 0.29, 1.44, 3.84, 27.8],
-        10: [0.19, 0.29, 0.86, 2.88, 9.6, 72.9],
-        12: [0.19, 0.19, 0.67, 1.92, 7.77, 23.0, 163.1],
-        14: [0.19, 0.19, 0.29, 1.82, 4.8, 17.3, 53.7, 403.0],
-        16: [0.19, 0.19, 0.19, 1.92, 3.84, 8.64, 24.9, 124.7, 959.5]
-      }
-    }
-
-    if (multipliers?.[risk] && multipliers?.[risk]?.[row]) {
-      return multipliers[risk][row]
-    }
-    return []
-  }
-  const handleCollision = (event: IEventCollision<Engine>) => {
-    const { pairs } = event
-
-    pairs.forEach((pair, i) => {
-      const { bodyA, bodyB } = pair
-      const { label: labelA } = bodyA
-      const { label: labelB } = bodyB
-
-      if (labelA !== labelB) {
-        if (labelB === 'plinko') {
-          if (!ballCache[bodyB.id]) {
-            ballCache[bodyB.id] = 0
-          }
-          ballCache[bodyB.id]++
-
-          const shiftedX = (mainConfig.width / 2 - bodyB.position.x) % (columnSize / 2)
-          const shiftedY =
-            (-bodyB.position.y + (16 - (rowSettings.pegSize + rowSettings.plinkoSize))) % rowSize
-          const newX =
-            Math.abs(shiftedX) < columnSize / 4
-              ? shiftedX
-              : columnSize / 2 + shiftedX * (shiftedX < 0 ? 1 : -1)
-
-          const newY = shiftedY
-          Body.setPosition(bodyB, {
-            x: bodyB.position.x + newX,
-            y: bodyB.position.y + newY
-          })
-
-          forceCache.push({
-            body: bodyB,
-            force: {
-              x: rowSettings.xForce * (paths[bodyB.id][ballCache[bodyB.id] - 1] === 1 ? 1 : -1),
-              y: rowSettings.yForce
-            }
-          })
-
-          if (
-            labelA === 'BottomWall' ||
-            labelA === 'Rectangle Body' ||
-            labelA === 'LeftWall' ||
-            labelA === 'RightWall'
-          ) {
-            const rights = paths[bodyB.id].filter((val: number) => val === 1).length
-            const i = (rights - (paths[bodyB.id].length - rights)) / 2 + paths[bodyB.id].length / 2
-            const multiplierBox2 = multiplierRefs.current[i]
-            console.log(multiplierBox2)
-
-            if (multiplierBox2?.style) {
-              multiplierBox2.style.transform = 'translateY(10px)'
-
-              setTimeout(() => {
-                multiplierBox2.style.transform = 'translateY(0px)'
-              }, 1000)
-            }
-
-            World.remove(engine.world, bodyB)
-            delete paths[bodyB.id]
-
-            return
-          }
-        }
-      }
     })
   }
 
@@ -304,13 +272,15 @@ const PlinkoGame2 = ({ rows, risk, numberOfBets }: PlinkoGame2Props) => {
             <div
               key={multiplier + new Date().getTime() * Math.random()}
               className={clsx(
-                `${getColorByMultiplier(multiplier)} flex items-center justify-center  rounded`,
+                `${getColorByMultiplier(
+                  multiplier
+                )} flex items-center justify-center rounded mx-0.5`,
                 {
-                  'h-4 text-[8px] px-1.5 mx-0.5': rows === 16,
-                  'h-4 text-10 px-1.5 mx-0.5': rows === 14,
-                  'h-5 text-11 px-2 mx-0.5': rows === 12,
-                  'h-7 text-14 px-2 mx-0.5': rows === 10,
-                  'h-8 text-14 px-3 mx-1': rows === 8
+                  'h-4 text-[8px] w-7': rows === 16,
+                  'h-4 text-10 w-8': rows === 14,
+                  'h-5 text-11 w-9': rows === 12,
+                  'h-7 text-13 w-11': rows === 10,
+                  'h-8 text-14 w-14': rows === 8
                 }
               )}
               id={`mult_${index}`}
