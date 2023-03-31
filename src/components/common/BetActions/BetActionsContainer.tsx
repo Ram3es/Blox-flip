253e5c20edff939fb9bf10cebb5b5f9d226a2891
Alexey Tsukanov <alexey.t@itsua.org>
2023-03-31 15:25:10 +0300

    fix problem

--- src/components/common/BetActions/BetActionsContainer.tsx
+++ src/components/common/BetActions/BetActionsContainer.tsx
@@ -11,7 +11,7 @@ const BetActionsContainer = ({ children, isBlocked = false }: BetActionsContaine
     <div
       className={`${
         isBlocked ? 'pointer-events-none' : 'pointer-event-auto'
-      } bg-blue-accent rounded-lg py-5 flex flex-col justify-between space-y-5 xxs:w-full min-w-[298px]`}
+      } bg-blue-accent rounded-lg py-5 flex flex-col justify-between space-y-5 xxs:w-full 2xl:w-[298px]`}
     >
       <div className='mx-4'>{children}</div>
       <div className='flex items-end justify-center text-blue-golf pb-4'>
