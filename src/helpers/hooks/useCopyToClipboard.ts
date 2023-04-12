b5203e2adb1790268fd4112163571f8a6d0d8182
Andrii Rastorhuiev <andrey.r@itsua.org>
2023-04-12 11:51:16 +0300

    fix hook

--- src/helpers/hooks/useCopyToClipboard.ts
+++ src/helpers/hooks/useCopyToClipboard.ts
@@ -1,27 +1,14 @@
 import { useState, useCallback } from 'react'
 import { debounce } from './useDebounceCallback'
 
-type CopiedValue = string | undefined
-type CopyFn = () => Promise<boolean>
-
 const useCopyToClipboard = (initialText: string) => {
   const [text, setText] = useState(initialText)
-  const [copiedText, setCopiedText] = useState<CopiedValue>(initialText)
+  const [copiedText, setCopiedText] = useState<string | undefined>(initialText)
 
-  const handleCopyText: CopyFn = useCallback(
+  const handleCopyText = useCallback(
     debounce(async () => {
-      if (!navigator?.clipboard) {
-        return false
-      }
-
-      try {
-        await navigator.clipboard.writeText(text)
-        setCopiedText(text)
-        return true
-      } catch (error) {
-        setCopiedText(undefined)
-        return false
-      }
+      await navigator.clipboard.writeText(text)
+      setCopiedText(text)
     }, 400),
     [text]
   )
