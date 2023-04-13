b7be84f7529c14a98f4bee6459dea1729bf4e107
Andrii Rastorhuiev <andrey.r@itsua.org>
2023-04-13 11:59:50 +0300

    fix key

--- src/pages/King/KingGameHistoryList.tsx
+++ src/pages/King/KingGameHistoryList.tsx
@@ -10,7 +10,10 @@ const KingGameHistoryList = ({ games }: KingGameHistoryListProps) => {
   return (
     <div className='gap-3 flex flex-col'>
       {games.map((game) => (
-        <KingGameHistoryItem key={game.firstPlayer.username} game={game} />
+        <KingGameHistoryItem
+          key={game.firstPlayer.username + String(new Date().getMilliseconds() * Math.random() * 5)}
+          game={game}
+        />
       ))}
     </div>
   )
--- src/pages/King/KingGameInventoriesList.tsx
+++ src/pages/King/KingGameInventoriesList.tsx
@@ -10,7 +10,11 @@ const KingGameInventoriesList = ({ itemList }: KingGameInventoriesListProps) => 
   return (
     <div className='w-full flex flex-wrap overflow-y-auto h-[200px] xs:h-[380px] pr-2.5 scrollbar-thumb-blue-secondary scrollbar-track-blue-darken/40 scrollbar-thin scrollbar-track-rounded-full scrollbar-thumb-rounded-full'>
       {itemList.map((item) => (
-        <ItemCard variant='CoinFlipSmall' key={item.id} {...item} />
+        <ItemCard
+          variant='CoinFlipSmall'
+          key={item.id + String(new Date().getMilliseconds())}
+          {...item}
+        />
       ))}
     </div>
   )
