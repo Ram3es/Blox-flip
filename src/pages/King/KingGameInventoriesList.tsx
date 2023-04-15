a761b89e8fca1e1566dfdc58adcd7098a74ee68b
Andrii Rastorhuiev <andrey.r@itsua.org>
2023-04-15 21:55:28 +0300

    fix styles

--- src/pages/King/KingGameInventoriesList.tsx
+++ src/pages/King/KingGameInventoriesList.tsx
@@ -11,7 +11,7 @@ const KingGameInventoriesList = ({ itemList }: KingGameInventoriesListProps) => 
     <div className='w-full flex flex-wrap overflow-y-auto min-h-[200px] xs:h-[380px] px-1.5 scrollbar-thumb-blue-secondary scrollbar-track-blue-darken/40 scrollbar-thin scrollbar-track-rounded-full scrollbar-thumb-rounded-full'>
       {itemList.map((item) => (
         <ItemCard
-          variant='KingList'
+          variant='FourByFour'
           key={item.id + String(new Date().getMilliseconds())}
           {...item}
         />
