0018378d24bbdda36760c3d9728e47f2e66f9418
Roman Luzan <romaluzan1@gmail.com>
2023-04-19 15:26:30 +0300

    modal/trivia

--- src/components/containers/TriviaModal.tsx
+++ src/components/containers/TriviaModal.tsx
@@ -1,6 +1,32 @@
 import React from 'react'
 import ModalWrapper from './ModalWrapper'
 import Bulb from '../icons/Bulb'
+import FieldWithLabel from '../common/FieldWithLabel'
+import { QuantityCoinsWithChildren } from '../common/QuantityCoins/QuantityWithChildren'
+import { Button } from '../base/Button'
+
+const triviaFields = [
+  {
+    title: 'Amount',
+    content: <QuantityCoinsWithChildren quantity={1500} />
+  },
+  {
+    title: 'Question 1',
+    content: <span className='text-gray-primary'>What is 1+1?</span>
+  },
+  {
+    title: 'Answer 1',
+    content: <span className='text-green-primary'>2</span>
+  },
+  {
+    title: '(opt) Answer 1',
+    content: '...'
+  },
+  {
+    title: '(opt) Answer 2',
+    content: '...'
+  }
+]
 
 const TriviaModal = ({ isOpen = true, onClose }: { isOpen: boolean, onClose: Function }) => {
   return (
@@ -19,6 +45,35 @@ const TriviaModal = ({ isOpen = true, onClose }: { isOpen: boolean, onClose: Fun
             </div>
           </div>
           <div className='border-b border-blue-accent-primary w-full' />
+          <div className=' flex flex-col mt-6  gap-2'>
+            { triviaFields.map(field => (
+              <FieldWithLabel
+                key={field.title}
+                title={field.title}
+                labelVariant='INSIDE'
+                labelClasses={ field.title === 'Answer 1' ? 'text-green-primary bg-green-primary/20 py-1.5 px-4' : undefined }
+                >
+                  <div className='text-gray-primary'>{field.content}</div>
+                 </FieldWithLabel>
+            )) }
+          </div>
+          <div className='flex items-center justify-center gap-5 mt-6'>
+            <Button
+              className='flex items-center justify-center  min-w-[110px] leading-9 text-gray-primary text-sm font-bold rounded bg-blue-accent-primary hover:text-white px-2.5 '
+            >
+              Auto Trivia
+            </Button>
+            <Button
+              className='flex items-center justify-center  min-w-[110px] leading-9 text-sm font-bold rounded bg-green-primary hover:bg-green-highlight px-2.5'
+            >
+              Start Trivia
+            </Button>
+          </div>
+          <div className='flex flex-col gap-3 mt-6 px-9 py-6 gradient-trivia-info rounded-2xl text-[#7682B1] text-center text-base '>
+            <p>Please verity the questions and the answer, make sure there are no typos before hosting a trivia.</p>
+            <p className='text-[#CE6047] indent-6'>Make sure you follow the strict rules or you’ll be permanently banned from hosting or participating in these Trivias</p>
+            <p>Please verity the questions and the answer, make sure there are no typos before hosting a trivia.</p>
+          </div>
         </div>
       </ModalWrapper>
         )
