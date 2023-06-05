d26e639b9ef624342d0c0470f1359a5583251d59
Andrii Rastorhuiev <andrii.rastorhuiev@itsua.eu>
2023-06-05 11:41:15 +0300

    fix awatawith username

--- src/components/common/AvatarWithUsername.tsx
+++ src/components/common/AvatarWithUsername.tsx
@@ -1,20 +1,34 @@
 import { forwardRef } from 'react'
+import clsx from 'clsx'
+
 import Image from '../base/Image'
+import QuestionIcon from '../icons/QuestionIcon'
 
 interface AvatarWithUsernameProps {
-  avatar: string
-  username: string
+  avatar?: string
+  username?: string
 }
 
 const AvatarWithUsername = forwardRef<HTMLDivElement, AvatarWithUsernameProps>(
   ({ avatar, username }, ref) => {
     return (
       <div ref={ref} className='flex items-center space-x-2'>
-        <Image
-          className='w-11 h-10 border border-blue-highlight rounded radial--blue'
-          image={avatar}
-        />
-        <span className='font-bold text-sm hidden md:block'>{username ?? '...'}</span>
+        <div className='w-11 h-10 border border-blue-highlight rounded radial--blue flex items-center justify-center'>
+          {avatar && <Image className='w-full h-full rounded' image={avatar} />}
+          {!avatar && (
+            <span className='text-blue-highlight-third'>
+              <QuestionIcon />
+            </span>
+          )}
+        </div>
+
+        <span
+          className={clsx('w- font-bold text-sm hidden md:block w-20 truncate', {
+            'text-blue-ocean-secondary': !username
+          })}
+        >
+          {username ?? '...'}
+        </span>
       </div>
     )
   }
