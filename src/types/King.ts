2bd968aeebecdd02a0b7740924ad09cbbdbe2860
Andrii Rastorhuiev <andrii.rastorhuiev@itsua.eu>
2023-06-08 11:37:28 +0300

    add types,

--- src/types/King.ts
+++ src/types/King.ts
@@ -1,17 +1,45 @@
 import { IItemCard } from './ItemCard'
 
-export interface IKingGamePlayer {
-  username: string
-  items: IItemCard[]
+export interface IKingPlayer {
+  id: string
+  name: string
+  avatar: string
+  level: number
+  value: number
+  players_skins: IItemCard[]
 }
 
-export interface IKingGame {
-  firstPlayer: IKingGamePlayer
-  secondPlayer?: IKingGamePlayer
+export interface IKingChampion {
+  id: string
+  round: number
+  champion: IKingPlayer
+  challenger: IKingPlayer
+  end: number
+  time: number
 }
+
+export interface IKingHistory {
+  id: string
+  round: number
+  hash: string
+  champion: IKingPlayer
+  challenger: IKingPlayer
+  winner: IKingPlayer
+}
+
+export interface IKingWeapon {
+  1: 'sword'
+  2: 'knife'
+  3: 'flaming_sword'
+  4: 'pistol'
+  5: 'automatic_weapon'
+}
+
+export type IKingWeaponKey = keyof IKingWeapon
+export type IKingAnimationType = IKingWeapon[IKingWeaponKey]
 
 export interface IKingFight {
-  by: 'opponent' | 'king'
-  animation: 'spritesheet'
-  damage: number
+  attacker: 'champion' | 'challenger'
+  weapon: IKingWeaponKey
+  attack: number
 }
