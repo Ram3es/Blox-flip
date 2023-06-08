30009dc62c2503aae19b22b9ba5997b1211f9fff
Andrii Rastorhuiev <andrii.rastorhuiev@itsua.eu>
2023-06-08 11:39:37 +0300

    add mock, add helper

--- src/helpers/kingHelpers.ts
+++ src/helpers/kingHelpers.ts
@@ -1,6 +1,23 @@
+import { IKingAnimationType, IKingWeaponKey } from '../types/King'
+
 export const getPercentByDamage = (damage: number, maxHealthPoints: number) => {
   return ((damage * 100) / maxHealthPoints).toFixed(2)
 }
 export const getFightDuration = (roundDuration: number, fightRounds: number): number => {
   return roundDuration * fightRounds
+}
+
+export const getAnimationByWeaponKey = (weaponKey: IKingWeaponKey): IKingAnimationType => {
+  switch (weaponKey) {
+    case 1:
+      return 'sword'
+    case 2:
+      return 'knife'
+    case 3:
+      return 'flaming_sword'
+    case 4:
+      return 'pistol'
+    case 5:
+      return 'automatic_weapon'
+  }
 }
--- src/mocks/kingMock.ts
+++ src/mocks/kingMock.ts
@@ -1,7 +1,7 @@
-import { IKingGame } from '../types/King'
+import { IKingHistory, IKingPlayer } from '../types/King'
 import { cards } from './cards'
 
-export const kingMock: IKingGame[] = [
+export const kingMock = [
   {
     firstPlayer: {
       username: 'test1',
@@ -54,7 +54,7 @@ export const kingMock: IKingGame[] = [
   }
 ]
 
-export const kingMockInitial: IKingGame = {
+export const kingMockInitial = {
   firstPlayer: {
     username: 'test1',
     items: cards.slice(0, 9)
@@ -64,3 +64,65 @@ export const kingMockInitial: IKingGame = {
     items: cards.slice(0, 8)
   }
 }
+
+const playerMock: IKingPlayer = {
+  id: '232rqwsrqe',
+  name: 'Arbuz',
+  avatar:
+    'https://avatars.cloudflare.steamstatic.com/063dc7e51bde42a6b4580e976a70efa19fb12e73_full.jpg',
+  level: 5,
+  value: 2.24,
+  players_skins: cards.slice(0, 5)
+}
+
+const playerSecondMock: IKingPlayer = {
+  id: '23211rq242wsrqe',
+  name: 'Baklajan',
+  avatar: 'https://steamavatar.io/img/1477742850r3v3p.jpg',
+  level: 5,
+  value: 2.24,
+  players_skins: cards.slice(0, 5)
+}
+
+export const kingHistoryMock: IKingHistory[] = [
+  {
+    id: 'blalb12412',
+    round: 1,
+    hash: 'rqwrqwrqwrqwrojweth239h23i4un',
+    champion: playerMock,
+    challenger: playerSecondMock,
+    winner: playerSecondMock
+  },
+  {
+    id: 'blalb12412',
+    round: 1,
+    hash: 'rqwrqwrqwrqwrojweth239h23i4un',
+    champion: playerMock,
+    challenger: playerSecondMock,
+    winner: playerSecondMock
+  },
+  {
+    id: 'blalb12412',
+    round: 1,
+    hash: 'rqwrqwrqwrqwrojweth239h23i4un',
+    champion: playerMock,
+    challenger: playerSecondMock,
+    winner: playerSecondMock
+  },
+  {
+    id: 'blalb12412',
+    round: 1,
+    hash: 'rqwrqwrqwrqwrojweth239h23i4un',
+    champion: playerMock,
+    challenger: playerSecondMock,
+    winner: playerSecondMock
+  },
+  {
+    id: 'blalb12412',
+    round: 1,
+    hash: 'rqwrqwrqwrqwrojweth239h23i4un',
+    champion: playerMock,
+    challenger: playerSecondMock,
+    winner: playerSecondMock
+  }
+]
