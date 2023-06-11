20b3f491f03340a603340587cdecd53f12d22281
Roman Luzan <romaluzan1@gmail.com>
2023-06-11 21:58:35 +0300

    load-king-sprites

--- src/constants/king.ts
+++ src/constants/king.ts
@@ -1,3 +1,9 @@
+import blade from '../assets/img/king/king-sprite-blade.png'
+import cosmic from '../assets/img/king/king-sprite-cosmic-guard.png'
+import cowboy from '../assets/img/king/king-sprite-cowboy.png'
+import iceknight from '../assets/img/king/king-sprite-iceknight.png'
+import knight from '../assets/img/king/king-sprite-knight.png'
+
 export const ROUND_DURATION = 5
 export const ROUND_DURATION_MILLISECONDS = ROUND_DURATION * 1000
 
@@ -6,3 +12,11 @@ export const TIME_EFFECT_MILLISECONDS = TIME_EFFECT * 1000
 
 export const HEALTH_POINTS_EFFECT = 1.5
 export const HEALTH_POINTS_EFFECT_MILLISECONDS = HEALTH_POINTS_EFFECT * 1000
+
+export const LIST_SPRITES: string[] = [
+  blade,
+  cosmic,
+  cowboy,
+  knight,
+  iceknight
+]
--- src/pages/King/King.tsx
+++ src/pages/King/King.tsx
@@ -61,7 +61,7 @@ const King = () => {
   return (
     <div className='ls:mt-20 flex flex-col justify-center gap-4 mx-4'>
       <div className='flex justify-end md:mr-36 md:mb-2'>
-        <VerifyBets path='/provably-fair#king' />
+        <VerifyBets path='/provably-fair#champion' />
       </div>
       <KingArena game={kingGame} fight={kingFight} setFight={setKingFight} />
       <div className='items-center justify-center flex gap-2'>
