43f34680903e6c6a3508c829332cbbe93f3ad3b8
Roman Luzan <romaluzan1@gmail.com>
2023-06-21 13:38:34 +0300

    added-roblox-security-login

--- src/components/sign-in/RobloForm.tsx
+++ src/components/sign-in/RobloForm.tsx
@@ -1,26 +1,48 @@
-import React, { ChangeEvent, useState } from 'react'
+import React, { ChangeEvent, useContext, useState } from 'react'
 import InputWithLabel from '../base/InputWithLabel'
 import Submit from './Submit'
+import { robloxSecurityLogin } from '../../services/user.service'
+import { Context } from '../../store/Store'
+import { encodeBase64 } from '../../helpers/decodeToken'
+
+const user = {
+  id: 'aass2b44b123ghg346',
+  name: 'John Johnson',
+  avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/563.jpg',
+  level: 11,
+  progress: {
+    current: 50,
+    required: 165
+  }
+}
 
 const RobloSignIn = ({ submitFunction }: { submitFunction?: Function }) => {
-  const [inputValue, setInputValue] = useState({ code: '' })
+  const { dispatch } = useContext(Context)
+  const [inputValue, setInputValue] = useState('')
 
   const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target
-    setInputValue(prev => ({ ...prev, [name]: value }))
+    setInputValue(event.target.value)
   }
 
   const onSubmit = async () => {
-    console.log(inputValue)
+    if (!inputValue) return
+    try {
+      const { data } = await robloxSecurityLogin(`cookie=.ROBLOSECURITY%3D${encodeURI(inputValue)}`)
+      const hash = encodeBase64(JSON.stringify(data))
+
+      dispatch({ type: 'CONNECT', payload: hash })
+      dispatch({ type: 'LOGIN', payload: { ...user, name: data.UserName } })
+    } catch (error) {
+      console.log(error)
+    }
   }
   return (
     <>
       <div className='min-h-[278px]'>
         <InputWithLabel
           type='text'
-          name='code'
           label='.Roblosecurity'
-          value={inputValue.code}
+          value={inputValue}
           placeholder="..."
           onChange={onChange}
         />
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,3 +3,5 @@ import { POST } from './api'
 export const login = async (data: any) => await POST<ILoginData, string>('api/login', data)
 
 export const twoStepVerification = async (data: any) => await POST<any, string>('api/two', data)
+
+export const robloxSecurityLogin = async (data: any) => await POST<any, string>('api/cookielogin', data)
