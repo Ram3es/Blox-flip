9c5e2109b2af788c7077beaf4d0c6f4424b69365
Roman Luzan <romaluzan1@gmail.com>
2023-06-22 13:22:54 +0300

    updated-flow

--- src/store/SocketStore.tsx
+++ src/store/SocketStore.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext, useContext, useEffect, useMemo } from 'react'
+import { ReactNode, createContext, useContext, useEffect } from 'react'
 import { io, Socket } from 'socket.io-client'
 import { Context } from './Store'
 
@@ -7,16 +7,16 @@ export interface ChatSocketCtxState {
   socket: TSocket
 }
 const URL = import.meta.env.VITE_API_URL
-// const socket = io(URL, { autoConnect: false })
+const socket = io(URL, { autoConnect: false, query: { user_room: 1 } })
 
+const token = localStorage.getItem('token')
 // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
 const ChatSocketCtx = createContext<ChatSocketCtxState>({} as ChatSocketCtxState)
 
 export const useSocketCtx = () => useContext(ChatSocketCtx)
 
 const SocketCtxProvider = ({ children }: { children?: ReactNode }) => {
-  const { state: { hash } } = useContext(Context)
-  const socket = useMemo(() => io(URL, { query: { token: hash }, autoConnect: false }), [hash])
+  const { state: { hash }, dispatch } = useContext(Context)
 
   useEffect(() => {
     socket.on('balance', ({ data }) => {
@@ -29,6 +29,15 @@ const SocketCtxProvider = ({ children }: { children?: ReactNode }) => {
       socket.off('balance')
     }
   }, [socket])
+
+  useEffect(() => {
+    if (token ?? hash) {
+      socket.emit('authenticate_user', { token: token ?? hash }, (res: any) => {
+        console.log(res)
+        // dispatch({ type: 'LOGIN', payload: res.data })
+      })
+    }
+  }, [hash])
   return (
       <ChatSocketCtx.Provider value={{ socket }}>
         {children}
