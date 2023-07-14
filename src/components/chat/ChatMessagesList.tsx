8522ac5a15b91578c231783dfff3f3b5847c3459
Andrii Rastorhuiev <andrii.rastorhuiev@itsua.eu>
2023-07-14 18:45:15 +0300

    fix chat

--- src/components/chat/ChatMessagesList.tsx
+++ src/components/chat/ChatMessagesList.tsx
@@ -3,7 +3,6 @@ import { ChatMessage } from './ChatMessage'
 
 export const ChatMessageList = () => {
   const { historyChat } = useChat()
-
   return (
     <div className='grow relative chat--gradient'>
       <div className='absolute inset-0 bottom-14 overflow-auto scrollbar-thumb-blue-secondary scrollbar-track-blue-darken/40 scrollbar-thin scrollbar-track-rounded-full scrollbar-thumb-rounded-full pr-3 -mr-2'>
