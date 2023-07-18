0b1d8b620cdc3cd974822aeea237e06ebc267d82
Andrii Rastorhuiev <andrii.rastorhuiev@itsua.eu>
2023-07-18 13:05:41 +0300

    add new plinko logic, add plinko socket

--- src/pages/Plinko/PlinkoActions.tsx
+++ src/pages/Plinko/PlinkoActions.tsx
@@ -13,13 +13,17 @@ import { Button } from '../../components/base/Button'
 import { BetToolkit } from '../../types/Bets'
 import { BetMode } from '../../types/Plinko'
 import { debounce } from '../../helpers/hooks/useDebounceCallback'
-import { getRandomPathByRows } from '../../helpers/plinkoHelpers'
+import { getRandomValidPath } from '../../helpers/plinkoHelpers'
 import { PlinkoConfig } from '../../constants/plinko'
 import DiamondIcon from '../../components/icons/DiamondIcon'
+import { useSocketCtx } from '../../store/SocketStore'
+import { getToast } from '../../helpers/toast'
 
 const PlinkoActions = () => {
   const [selectedBet, setSelectedBet] = useState<BetToolkit | null>(null)
   const [isButtonDisabled, setIsButtonDisabled] = useState(false)
+
+  const { socket } = useSocketCtx()
 
   const {
     isStarted,
@@ -100,13 +104,30 @@ const PlinkoActions = () => {
       if (numberOfBets >= 1) {
         mode === 'Automatic' && buttonDisabled()
 
-        setInGameBalls((prev: number) => (numberOfBets > 1 ? (prev += numberOfBets) : prev + 1))
-        setPaths([])
-        setIsStarted(true)
+        socket.emit(
+          'plinko',
+          { rows: selectedRow, risk, wager: betAmount },
+          (
+            err: boolean,
+            { multiplier, amount, win }: { multiplier: number; amount: number; win: number }
+          ) => {
+            if (err) {
+              getToast("Plinko doesn't fall")
+            }
 
-        for (let index = 0; index < numberOfBets; index++) {
-          setPaths((prev) => [...prev, getRandomPathByRows(selectedRow)])
-        }
+            if (!err) {
+              setInGameBalls((prev: number) =>
+                numberOfBets > 1 ? (prev += numberOfBets) : prev + 1
+              )
+              setPaths([])
+              setIsStarted(true)
+
+              const validPath = getRandomValidPath(risk, selectedRow, multiplier)
+
+              setPaths((prev) => [...prev, validPath])
+            }
+          }
+        )
       }
     }, 400),
     [numberOfBets, isStarted, selectedRow]
@@ -119,24 +140,24 @@ const PlinkoActions = () => {
   }, [isStarted, inGameBalls])
 
   return (
-    <BetActionsContainer path='/provably-fair#plinko'>
+    <BetActionsContainer path="/provably-fair#plinko">
       <ToggleMode mode={mode} handleChange={handleChangeBetMode} />
-      <div className='flex flex-col space-y-7 mt-4'>
-        <div className='border-b-2 border-b-blue-accent-fourth pb-6'>
-          <div className='relative'>
+      <div className="flex flex-col space-y-7 mt-4">
+        <div className="border-b-2 border-b-blue-accent-fourth pb-6">
+          <div className="relative">
             <InputWithLabel
-              type='number'
-              labelClasses='flex flex-col w-full mb-8 items-start'
-              titleClasses='gradient-blue-secondary text-gray-primary text-sm px-4 py-3 leading-4 rounded-t-xl inline-block'
-              inputWrapperClasses='bg-dark/25 rounded-b-xl rounded-tr-xl overflow-hidden w-full'
-              inputClasses='grow w-0 mr-2 bg-transparent bg-none border-none outline-none shadow-none pl-8'
-              name='password'
-              label='Bet amount'
+              type="number"
+              labelClasses="flex flex-col w-full mb-8 items-start"
+              titleClasses="gradient-blue-secondary text-gray-primary text-sm px-4 py-3 leading-4 rounded-t-xl inline-block"
+              inputWrapperClasses="bg-dark/25 rounded-b-xl rounded-tr-xl overflow-hidden w-full"
+              inputClasses="grow w-0 mr-2 bg-transparent bg-none border-none outline-none shadow-none pl-8"
+              name="password"
+              label="Bet amount"
               value={betAmount}
               onChange={handleChangeBetAmount}
             />
-            <div className='w-6 h-6 text-center leading-6 shrink-0 bg-green-primary/20 rounded mr-2 text-green-primary absolute top-14 left-4'>
-              <DiamondIcon className='-inset-full absolute m-auto' />
+            <div className="w-6 h-6 text-center leading-6 shrink-0 bg-green-primary/20 rounded mr-2 text-green-primary absolute top-14 left-4">
+              <DiamondIcon className="-inset-full absolute m-auto" />
             </div>
           </div>
           <RangeSlider
@@ -146,10 +167,10 @@ const PlinkoActions = () => {
             sliderValueChanged={handleChangeBetAmount}
           />
         </div>
-        <div className='border-b-2 border-b-blue-accent-fourth pb-6'>
+        <div className="border-b-2 border-b-blue-accent-fourth pb-6">
           <ToggleBets value={selectedBet} handleChange={setSelectedBet} betToolkit={betToolkit} />
         </div>
-        <div className='border-b-2 border-b-blue-accent-fourth flex flex-col w-full pb-6'>
+        <div className="border-b-2 border-b-blue-accent-fourth flex flex-col w-full pb-6">
           <ToggleRows
             isBlocked={isStarted}
             value={selectedRow}
@@ -157,26 +178,26 @@ const PlinkoActions = () => {
             rowOptions={rowOptions}
           />
         </div>
-        <div className='border-b-2 border-b-blue-accent-fourth pb-6'>
+        <div className="border-b-2 border-b-blue-accent-fourth pb-6">
           <ToggleRisk isBlocked={isStarted} value={risk} handleChange={setRisk} />
         </div>
         {mode === 'Automatic' && (
-          <div className='border-b-2 border-b-blue-accent-fourth'>
+          <div className="border-b-2 border-b-blue-accent-fourth">
             <InputWithLabel
-              type='number'
-              name='numberOfBets'
-              label='Number of Bets'
-              placeholder='Enter number of bets'
+              type="number"
+              name="numberOfBets"
+              label="Number of Bets"
+              placeholder="Enter number of bets"
               value={numberOfBets}
               onChange={(e) => setNumberOfBets(Number(e.target.value))}
             />
           </div>
         )}
-        <div className='flex pb-6'>
+        <div className="flex pb-6">
           <Button
             disabled={isButtonDisabled}
             onClick={handlePlaceBet}
-            className='w-full bg-green-primary rounded h-11'
+            className="w-full bg-green-primary rounded h-11"
           >
             Place Bet
           </Button>
