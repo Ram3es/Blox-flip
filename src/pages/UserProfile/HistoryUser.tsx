2c9798c6950c5cab34cbb7a31c3c99d52a8c7037
Roman Luzan <romaluzan1@gmail.com>
2023-09-19 18:27:07 +0300

    upd-wheel

---
+++ src/pages/UserProfile/HistoryUser.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { History } from '../../components/history/History'
+
+const HistoryUser = () => {
+  return (
+    <div className='h-screen'>
+      <History />
+    </div>
+  )
+}
+
+export default HistoryUser
