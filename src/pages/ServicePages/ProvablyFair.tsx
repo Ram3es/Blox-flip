0350700f26f5e4f80b90ac5bbb165998b6763ff4
Roman Luzan <romaluzan1@gmail.com>
2023-09-22 18:35:15 +0300

    change text

--- src/pages/ServicePages/ProvablyFair.tsx
+++ src/pages/ServicePages/ProvablyFair.tsx
@@ -1,5 +1,5 @@
-/* eslint-disable no-template-curly-in-string */
-import React, { useEffect, useRef } from 'react'
+
+import { useEffect, useRef } from 'react'
 import { useLocation } from 'react-router-dom'
 import ServicePageContainer from '../../components/containers/ServicePage'
 import { games } from '../../constants/provably-fair'
@@ -27,25 +27,81 @@ const ProvablyFair = () => {
         </>
     )}>
       <div ref={anchorRef}>
-        {games.map((game) => (<React.Fragment key={game.id}>
-            <h2 className='text-xl font-bold text-blue-golf mt-10 mb-6'>{game.title}</h2>
-            <div className='text-base leading-5 font-normal opacity-60 max-w-[850px]'>
-              <p className='mb-5'>In Plinko, our system generates a ticket based on your server seed, client seed and nonce. </p>
-              <div id={game.id} className='ml-1 mb-5'>
-                <p>1. Server seed ― a SHA-256 hash of 16 cryptographically secure random bytes. This seed can be cycled by the user at any time.</p>
-                <p>2.  Client seed ― a seed that each user can customize at any time.</p>
-                <p>3.  Nonce ― The round number that increments with each bet.</p>
-              </div>
-              <p>{'A hashed version of your current and next server seed is available at any time on the plinko page, and can be cycled at any time. You can only unhash a game\'s server seed when you cycle your seeds, and the game\'s used server seed is no longer in use.'}</p>
+          <h2 className='text-xl font-bold text-blue-golf mt-10 mb-6'>{games.plinko.title}</h2>
+          <div className='text-base leading-5 font-normal opacity-60 max-w-[850px]'>
+            <p className='mb-5'>On Plinko and Case Opening, we use a Random.ORG Generated Server Seed and a nonce system to generate the outcome.</p>
+            <div id={games.plinko.id} className='ml-1 mb-5'>
+              <p>1: We reach out to Random.ORG to generate your Server Seed. You can change this with a Click of a Button above after a few games.</p>
+              <p>2: Nonce: A number that is increased by each Plinko you play or each Case you open.</p>
             </div>
-            <div className='flex flex-col gap-5 p-8 bg-black/40 w-full text-blue-text-secondary text-base my-5 whitespace-pre'>
-              <p>{'const crypto = require(\'crypto\'); const hashString = (string) =>'}</p>
-              <p>{'crypto.createHash(\'sha256\').update(string).digest(\'hex\'); const generateDiceTicket = \n(serverSeed, publicSeed, nonce) => { const hash = '}</p>
-              <p>{'hashString(`${serverSeed}:${publicSeed}:${nonce}`); return parseInt(hash.substring\n(0, 8), 16) % 10000; }; const SERVER_SEED ='}</p>
-              <p>{'\'04093dcb0d2790612ecd6d9837e2e3c6144f30f16bf560357ecc14ac0f0236e3\'; const \n CLIENT_SEED = \'my client seed\'; const NONCE = 1; '}</p>
-              <p>console.log(generateDiceTicket(SERVER_SEED, CLIENT_SEED, NONCE));</p>
+            <p>We make a Generation String with these valuables and use Chance.JS to get the result.</p>
+          </div>
+          <div className='flex flex-col gap-5 p-8 bg-black/40 w-full text-blue-text-secondary text-base my-5 whitespace-pre'>
+            <p>{'Seed Generation String: random_org_seed+i;'}</p>
+            <p>{'const generation=new chance(seed); // random_org_seed+i; '}</p>
+            <p>{'Example:mowltkdlmvnhk4a1'}</p>
+            <p>{'const roll=generation.floating({min: 0, max: 100, fixed: 9});'}</p>
+          </div>
+          <h2 className='text-xl font-bold text-blue-golf mt-10 mb-6'>{games.wheel.title}</h2>
+          <div className='text-base leading-5 font-normal opacity-60 max-w-[850px]'>
+            <p className='mb-5'>On Wheel, we generate a Server Seed before the timer starts and a Random.ORG Seed is generated when it ends.</p>
+            <div id={games.wheel.id} className='ml-1 mb-5'>
+              <p>{'If the Wheel has no active users we don\'t reach out to Random.ORG, we replace their seed with "empty_game" for the generation to save resources.'}</p>
             </div>
-          </React.Fragment>))}
+          </div>
+          <div className='flex flex-col gap-5 p-8 bg-black/40 w-full text-blue-text-secondary text-base my-5 whitespace-pre'>
+            <p>{'Seed Generation String: server_seed; // If the round had no users'}</p>
+            <p>{'Seed Generation String: server_seed+random_org_seed; // If the round had any wagers'}</p>
+            <p>{'const generation=new chance(seed);'}</p>
+            <p>{'const roll=generation.integer({ min: 0, max: 53 });'}</p>
+          </div>
+          <h2 className='text-xl font-bold text-blue-golf mt-10 mb-6'>{games.jackpot.title}</h2>
+          <div className='text-base leading-5 font-normal opacity-60 max-w-[850px]'>
+            <p className='mb-5'>On Jackpot, we generate a Server Seed before the timer starts and a Random.ORG Seed is generated when it ends.</p>
+            <div id={games.jackpot.id} className='ml-1 mb-5'/>
+          </div>
+          <div className='flex flex-col gap-5 p-8 bg-black/40 w-full text-blue-text-secondary text-base my-5 whitespace-pre'>
+            <p>{'Seed Generation String: server_seed+random_org_seed;'}</p>
+            <p>{'const generation=new chance(seed);'}</p>
+            <p>{'const roll=generation.floating({min: 0, max: 100, fixed: 6});'}</p>
+          </div>
+          <h2 className='text-xl font-bold text-blue-golf mt-10 mb-6'>{games.coinflip.title}</h2>
+          <div className='text-base leading-5 font-normal opacity-60 max-w-[850px]'>
+            <p className='mb-5'>{'On Coinflip, we generate a Server Seed when it\'s created and a Random.ORG Seed is generated after the Challenger joins.'}</p>
+            <div id={games.coinflip.id} className='ml-1 mb-5'/>
+          </div>
+          <div className='flex flex-col gap-5 p-8 bg-black/40 w-full text-blue-text-secondary text-base my-5 whitespace-pre'>
+            <p>{'Seed Generation String: server_seed+random_org_seed;'}</p>
+            <p>{'const generation=new chance(seed); '}</p>
+            <p>{'const roll=generation.floating({min: 0, max: 100, fixed: 6});'}</p>
+          </div>
+          <h2 className='text-xl font-bold text-blue-golf mt-10 mb-6'>{games.cases.title}</h2>
+          <div className='text-base leading-5 font-normal opacity-60 max-w-[850px]'>
+            <p className='mb-5'>On Plinko and Case Opening, we use a Random.ORG Generated Server Seed and a nonce system to generate the outcome.</p>
+            <div id={games.cases.id} className='ml-1 mb-5'>
+              <p>1: We reach out to Random.ORG to generate your Server Seed. You can change this with a Click of a Button above after a few games.</p>
+              <p>2: Nonce: A number that is increased by each Plinko you play or each Case you open.</p>
+            </div>
+            <p>We make a Generation String with these valuables and use Chance.JS to get the result.</p>
+          </div>
+          <div className='flex flex-col gap-5 p-8 bg-black/40 w-full text-blue-text-secondary text-base my-5 whitespace-pre'>
+            <p>{'Seed Generation String: random_org_seed+i;'}</p>
+            <p>{'const generation=new chance(seed); // random_org_seed+i; '}</p>
+            <p>{'Example:mowltkdlmvnhk4a1'}</p>
+            <p>{'const roll=generation.integer({min: 0, max: 100000}); // We then compare this to the Odds on the Cases'}</p>
+          </div>
+          <h2 className='text-xl font-bold text-blue-golf mt-10 mb-6'>{games.battle.title}</h2>
+          <div className='text-base leading-5 font-normal opacity-60 max-w-[850px]'>
+            <p className='mb-5'>On Case Battles, we use a Random.ORG Generated Server Seed once the Game is full to generate the outcome.</p>
+            <p className='mb-5'>Each Case for Each User gets a new Roll, in Player Order.</p>
+            <div id={games.battle.id} className='ml-1 mb-5'/>
+          </div>
+          <div className='flex flex-col gap-5 p-8 bg-black/40 w-full text-blue-text-secondary text-base my-5 whitespace-pre'>
+            <p>{'Seed Generation String: random_org_seed+i;'}</p>
+            <p>{'const generation=new chance(seed); // random_org_seed+i; '}</p>
+            <p>{'Example:mowltkdlmvnhk4a1'}</p>
+            <p>{'const roll=generation.integer({min: 0, max: 100000}); // We then compare this to the Odds on the Cases'}</p>
+          </div>
         </div>
     </ServicePageContainer>
     </>
